69a30d4749fe42d3e0f47c2d9f0e59b9d38e02cb
Acelynn Qiao <aqiao@ucsd.edu>
2025-10-16 15:51:48 -0700

    Automate current page link with JavaScript

--- global.js
+++ global.js
@@ -1,5 +1,18 @@
-console.log('IT’S ALIVE!');
+console.log('IT\'S ALIVE!');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
+}
+
+// Step 2.1: Get all nav links
+const navLinks = $$("nav a");
+
+// Step 2.2: Find the link to the current page
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+// Step 2.3: Add the current class to the current page link
+if (currentLink) {
+  currentLink.classList.add('current');
 }
